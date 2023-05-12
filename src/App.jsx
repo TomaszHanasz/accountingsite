99ff0b0f3502b3acf109244422d9d1bc59e3697e
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-05-11 19:13:54 -0500

    count component update

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,6 @@ import React, { useState } from "react";
 import "./App.style.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const onClickHandlerIncrease = () => {
-    setCount(count + 10);
-    if (count >= 150) {
-      setCount(count);
-    }
-  };
-
-  const onClickHandlerDecrease = () => {
-    setCount(count - 10);
-    if (count === 0 || count > 150) {
-      setCount(count);
-    }
-  };
-
-  const onClickHandlerMultiply = () => {
-    setCount(count * 5);
-    if (count >= 150) {
-      setCount(count);
-    }
-  };
-
-  const onClickHandlerDivide = () => {
-    setCount(count / 5);
-    if (count < 1) {
-      setCount(count);
-    }
-  };
-
-  const onClickHandlerReset = () => {
-    setCount(0);
-  };
-
   return (
     <div className="App">
       <NavBar style={{ backgroundColor: `#00243f` }} />
@@ -48,14 +14,7 @@ function App() {
         text="See our experts with convenient filing options for your income tax preparation"
         image={headerImage}
       />
-      <h3 className="count-result">{count}</h3>
-      <div className="count-block">
-        <CountComponent buttonName={"+10"} onClick={onClickHandlerIncrease} />
-        <CountComponent buttonName={"-10"} onClick={onClickHandlerDecrease} />
-        <CountComponent buttonName={"Reset"} onClick={onClickHandlerReset} />
-        <CountComponent buttonName={"/ 5"} onClick={onClickHandlerDivide} />
-        <CountComponent buttonName={"x 5"} onClick={onClickHandlerMultiply} />
-      </div>
+      <CountComponent />
     </div>
   );
 }
--- src/components/countComponent/CountComponent.jsx
+++ src/components/countComponent/CountComponent.jsx
@@ -1,15 +1,81 @@
 import CustomButton from "../customButton/CustomButton";
+import React, { useState } from "react";
 import "./countComponent.style.css";
 
-const CountComponent = ({ onClick, count, buttonName }) => {
+const CountComponent = () => {
+  const [count, setCount] = useState(0);
+
+  const onClickHandlerIncrease = () => {
+    setCount(count + 10);
+    if (count >= 150) {
+      setCount(count);
+    }
+  };
+
+  const onClickHandlerDecrease = () => {
+    setCount(count - 10);
+    if (count <= 0) {
+      setCount(count);
+    }
+  };
+
+  const onClickHandlerMultiply = () => {
+    setCount(count * 5);
+    if (count >= 150) {
+      setCount(count);
+    }
+  };
+
+  const onClickHandlerDivide = () => {
+    setCount(count / 5);
+    if (count < 1) {
+      setCount(count);
+    }
+  };
+
+  const onClickHandlerReset = () => {
+    setCount(0);
+  };
   return (
-    <div className="count-component">
-      <p className="count-result">{count}</p>
-      <CustomButton
-        onClick={onClick}
-        buttonName={buttonName}
-        role={`count-button`}
-      />
+    <div className="count-result">
+      <h3>{count}</h3>
+      <div className="count-block">
+        <div className="count-component">
+          <CustomButton
+            onClick={onClickHandlerIncrease}
+            buttonName="+10"
+            role={`count-button`}
+          />
+        </div>
+        <div className="count-component">
+          <CustomButton
+            onClick={onClickHandlerDecrease}
+            buttonName="-10"
+            role={`count-button`}
+          />
+        </div>
+        <div className="count-component">
+          <CustomButton
+            onClick={onClickHandlerMultiply}
+            buttonName="x 5"
+            role={`count-button`}
+          />
+        </div>
+        <div className="count-component">
+          <CustomButton
+            onClick={onClickHandlerDivide}
+            buttonName="/ 5"
+            role={`count-button`}
+          />
+        </div>
+        <div className="count-component">
+          <CustomButton
+            onClick={onClickHandlerReset}
+            buttonName="Reset"
+            role={`count-button`}
+          />
+        </div>
+      </div>{" "}
     </div>
   );
 };
