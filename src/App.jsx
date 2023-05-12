1d537faf54ead20d1fc181d4685854c44738cefc
Tomasz Hanasz <tomaszhanasz@yahoo.pl>
2023-05-11 19:27:55 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <NavBar style={{ backgroundColor: `#00243f` }} />
       <CustomHeaderFullScreen
-        title="Filing taxes can be simple, secure & affordable"
+        title="Filing taxes can be simple, secure & affordable."
         text="See our experts with convenient filing options for your income tax preparation"
         image={headerImage}
       />
